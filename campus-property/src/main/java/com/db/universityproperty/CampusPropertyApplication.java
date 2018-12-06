e94a74be75f0ee66f8095df468624094793db554
pangalan <lozh.daria@gmail.com>
2018-12-06 22:56:49 +0700

    backend in particular module

---
+++ campus-property/src/main/java/com/db/universityproperty/CampusPropertyApplication.java
@@ -0,0 +1,12 @@
+package com.db.universityproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPropertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusPropertyApplication.class, args);
+    }
+}
---
+++ campus-property/src/test/java/com/db/universityproperty/CampusPropertyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.db.universityproperty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CampusPropertyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
