c5da6d740bc9b3f0db7cf55c2460f70d3ab2a761
pangalan <lozh.daria@gmail.com>
2018-12-09 23:45:45 +0700

    repositories

---
+++ campus-property/src/main/java/com/db/campus/property/dao/AccountantRepository.java
@@ -0,0 +1,10 @@
+package com.db.campus.property.dao;
+
+import com.db.campus.property.entity.AccountantEntity;
+import org.springframework.data.repository.Repository;
+
+public interface AccountantRepository extends Repository<AccountantEntity, Long> {
+
+
+
+}
---
+++ campus-property/src/main/java/com/db/campus/property/dao/RequestRepository.java
@@ -0,0 +1,10 @@
+package com.db.campus.property.dao;
+
+import com.db.campus.property.entity.RequestEntity;
+import org.springframework.data.repository.Repository;
+
+public interface RequestRepository extends Repository<RequestEntity, Long> {
+
+
+
+}
