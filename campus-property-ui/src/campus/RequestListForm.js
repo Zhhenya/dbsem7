386f4ee28b24bc6659c6fe6afdd17e6cd5cff713
pangalan <lozh.daria@gmail.com>
2018-12-11 12:18:05 +0700

    request list ui

---
+++ campus-property-ui/src/campus/RequestListForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import axios from "axios";
+import RequestListTable from "./RequestListTable";
+
+
+class RequestListForm extends Component {
+    state = {
+        data: [],
+    };
+
+    componentDidMount() {
+        this.fetchTableData();
+    }
+
+    fetchTableData = () =>  axios.get("/request/list").then(({ data }) => this.setState({ text: data }));
+
+    render() {
+        return(
+            <RequestListTable data={this.state.data}/>
+        );
+    }
+}
+
+export default RequestListForm;
