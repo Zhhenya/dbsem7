beee82ba29d41ca027b75c9f262e862f049b2f2b
pangalan <lozh.daria@gmail.com>
2018-12-25 17:00:18 +0700

    print table

--- campus-property-ui/src/App.js
+++ campus-property-ui/src/App.js
@@ -3,12 +3,15 @@ import { HashRouter } from "react-router-dom";
 import AppRouter from "./campus/AppRouter";
 import Auth from "./commons/Auth";
 import AppWrapper from "./campus/AppWrapper";
+import PrintProvider from "react-easy-print";
 
 const App = () => (
   <HashRouter>
     <Auth>
       <AppWrapper>
-        <AppRouter />
+        <PrintProvider>
+          <AppRouter />
+        </PrintProvider>
       </AppWrapper>
     </Auth>
   </HashRouter>
